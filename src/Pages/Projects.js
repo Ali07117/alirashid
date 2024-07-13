86b6244f0fb89e6ff73f933250e27dfa463d415a
Ali07117 <alirashid07117@gmail.com>
2024-07-13 15:38:42 +0500

    update

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -9,10 +9,10 @@ function Projects() {
     <div className='min-h-[100vh] w-[100%] pt-[80px] bg-[--secondry]  border-t-[2px]'>
         {elementdata.map((e)=>{
             return(
-
+             <a target='blank' href={e.websitelink}>
                 <div className="element h-[120px] w-[100%] border-b border-b-[1px] border-b-[--primary-text] -bg-[red] px-[100px] relative">
         <div className='elem-hover picture h-[280px] -bg-[white] border-[none] rounded-[10px] w-[230px] opacity-[0] fixed z-[20] left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%]'>
-            <img className='h-[100%] w-[100%]  object-contain ' src={e.hoverimg} alt="Brand image" />
+           <img className='h-[100%] w-[100%]  object-contain ' src={e.hoverimg} alt="Brand image" />
         </div>
         <div className="left -bg-[blue] h-[100%] flex justify-between items-center">
           <p className='elem-brand-name w-[250px]  text-[--primary-text] font-[500] text-[40px]'>{e.brand}</p>
@@ -20,6 +20,7 @@ function Projects() {
            <p className='elem-brand-used text-[--primary-text] w-[200px] font-[200]  -bg-[yellow]'>{e.brandused}</p>
         </div>
         </div> 
+        </a>
             )
         })}
     </div>
--- src/data/elemdata.js
+++ src/data/elemdata.js
@@ -4,34 +4,39 @@ export const elementdata = [
         "brand": "SUNDOWN",
         "branddesc": "Greate animated project. Through this project, I've honed my skills in coding, design, and creating engaging user experences.",
         "brandused": "HTML, CSS, Tailwind CSS Javascript, React js, Smooth scroll and animated libraries",
-        "hoverimg": "./assets/sundownlogo.svg"
+        "hoverimg": "./assets/sundownlogo.svg",
+        "websitelink": "https://www.sundown-studio.com/",
     },
     {
         "id": 2,
         "brand": "SIGNIFICO",
         "branddesc": "Developed a dynamic, scroll-animated project Significo, showcasing innovative UI elements and seamless user interaction.",
         "brandused": "HTML, CSS, Tailwind CSS Javascript, React js, Scroll-base animation and Smooth scroll",
-        "hoverimg": "./assets/significo.svg"
+        "hoverimg": "./assets/significo.svg",
+        "websitelink": "https://www.significo.com/",
     },
     {
         "id": 3,
         "brand": "McCann",
         "branddesc": "Created an engaging UI project McCann, Integrating modern design principles with intuitive user experience.",
         "brandused": "HTML, CSS, Tailwind CSS Javascript, React js, Scroll-base animation and Smooth scroll",
-        "hoverimg": "./assets/mccannlogo.svg"
+        "hoverimg": "./assets/mccannlogo.svg",
+        "websitelink": "https://www.mccann.fr/",
     },
     {
         "id": 4,
         "brand": "QRUIL",
         "branddesc": "One of the greatest project i have ever done with USA client on fiverr.",
         "brandused": "HTML, CSS, Tailwind CSS Javascript, React js and (some other libraries)",
-        "hoverimg": "./assets/qruillogo.svg"
+        "hoverimg": "./assets/qruillogo.svg",
+        "websitelink": "https://qruil.com/landing",
     },
     {
         "id": 5,
         "brand": "FUT NEXT",
         "branddesc": "Developed a clean and responsive UI for FUT NEXT, enhancing user interaction and overall experience.",
         "brandused": "HTML, CSS, Tailwind CSS Javascript, Next js and (some other libraries)",
-         "hoverimg": "./assets/futtnextlogo.svg"
+         "hoverimg": "./assets/futtnextlogo.svg",
+         "websitelink": "https://www.futnext.com/",
     },
 ]
