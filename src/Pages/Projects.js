6c17d82f0f41e72de1ddc38c3d383969390fb05b
Ali07117 <alirashid07117@gmail.com>
2024-07-13 15:16:19 +0500

    update

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -12,8 +12,7 @@ function Projects() {
 
                 <div className="element h-[120px] w-[100%] border-b border-b-[1px] border-b-[--primary-text] -bg-[red] px-[100px] relative">
         <div className='elem-hover picture h-[280px] -bg-[white] border-[none] rounded-[10px] w-[230px] opacity-[0] fixed z-[20] left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%]'>
-            <img className='h-[100%] w-[100%]  object-contain ' src="./public/assets/significo.svg" alt="ssss" />
-            {/* src={e.hoverimg} */}
+            <img className='h-[100%] w-[100%]  object-contain ' src={e.hoverimg} alt="Brand image" />
         </div>
         <div className="left -bg-[blue] h-[100%] flex justify-between items-center">
           <p className='elem-brand-name w-[250px]  text-[--primary-text] font-[500] text-[40px]'>{e.brand}</p>
