71f7ca2009e407534426ae9959622ea91a90c7e0
Ali07117 <alirashid07117@gmail.com>
2024-08-21 06:19:16 +0500

    update

--- src/data/elemdata.js
+++ src/data/elemdata.js
@@ -4,7 +4,7 @@ export const elementdata = [
         "brand": "SUNDOWN",
         "branddesc": "Greate animated project. Through this project, I've honed my skills in coding, design, and creating engaging user experences.",
         "brandused": "HTML, CSS, Tailwind CSS Javascript, React js, Smooth scroll and animated libraries",
-        "hoverimg": "/assets/sundownlogo.svg",
+        "hoverimg": "/sundownlogo.svg",
         "websitelink": "https://sundown-wine.vercel.app/",
     },
     {
@@ -12,7 +12,7 @@ export const elementdata = [
         "brand": "SIGNIFICO",
         "branddesc": "Developed a dynamic, scroll-animated project Significo, showcasing innovative UI elements and seamless user interaction.",
         "brandused": "HTML, CSS, Tailwind CSS Javascript, React js, Scroll-base animation and Smooth scroll",
-        "hoverimg": "./assets/significo.svg",
+        "hoverimg": "/significo.svg",
         "websitelink": "https://significo-five.vercel.app/",
     },
     {
@@ -20,7 +20,7 @@ export const elementdata = [
         "brand": "McCann",
         "branddesc": "Created an engaging UI project McCann, Integrating modern design principles with intuitive user experience.",
         "brandused": "HTML, CSS, Tailwind CSS Javascript, React js, Scroll-base animation and Smooth scroll",
-        "hoverimg": "./assets/mccannlogo.svg",
+        "hoverimg": "/mccannlogo.svg",
         "websitelink": "https://mccann-seven.vercel.app/",
     },
     {
@@ -28,7 +28,7 @@ export const elementdata = [
         "brand": "QRUIL",
         "branddesc": "One of the greatest project i have ever done with USA client on fiverr.",
         "brandused": "HTML, CSS, Tailwind CSS Javascript, React js and (some other libraries)",
-        "hoverimg": "./assets/qruillogo.svg",
+        "hoverimg": "/qruillogo.svg",
         "websitelink": "https://qruil.com/landing",
     },
     {
@@ -36,7 +36,7 @@ export const elementdata = [
         "brand": "FUT NEXT",
         "branddesc": "Developed a clean and responsive UI for FUT NEXT, enhancing user interaction and overall experience.",
         "brandused": "HTML, CSS, Tailwind CSS Javascript, Next js and (some other libraries)",
-         "hoverimg": "./assets/futtnextlogo.svg",
+         "hoverimg": "/futtnextlogo.svg",
          "websitelink": "https://www.futnext.com/",
     },
 ]
